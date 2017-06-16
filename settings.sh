3f0bb8c23e5545d6343f98217d3a79183a62d19f
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2017-06-16 17:59:41 -0500

    upgrade to latest lede release
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- settings.sh
+++ settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export genesisBuilder=lede-imagebuilder-17.01.1-ar71xx-generic.Linux-x86_64
-export genesisDownload=https://downloads.lede-project.org/releases/17.01.1/targets/ar71xx/generic/lede-imagebuilder-17.01.1-ar71xx-generic.Linux-x86_64.tar.xz
+export genesisBuilder=lede-imagebuilder-17.01.2-ar71xx-generic.Linux-x86_64
+export genesisDownload=https://downloads.lede-project.org/releases/17.01.2/targets/ar71xx/generic/lede-imagebuilder-17.01.2-ar71xx-generic.Linux-x86_64.tar.xz
 
