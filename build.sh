4ce23ec28975bbd039a110dfff5cc89251fc1913
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2017-06-17 21:04:16 -0500

    remove old comments
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- build.sh
+++ build.sh
@@ -8,8 +8,6 @@
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see http://www.gnu.org/licenses/.
 
-# MY_DIR=$(dirname $(readlink -f $0))
-# $MY_DIR/settings.sh
 . settings.sh
 
 #The LEDE image builder wants this umask set
