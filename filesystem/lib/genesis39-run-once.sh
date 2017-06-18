74359b832d99dfca91e5bc1985d0314de534f520
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2017-06-17 19:35:48 -0500

    don't auto include functions.sh
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- filesystem/lib/genesis39-run-once.sh
+++ filesystem/lib/genesis39-run-once.sh
@@ -7,7 +7,6 @@
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 # You should have received a copy of the GNU General Public License along with
 # this program. If not, see http://www.gnu.org/licenses/.
-. /lib/functions.sh
 
 genesis39_run_once() {
   local init_ran
