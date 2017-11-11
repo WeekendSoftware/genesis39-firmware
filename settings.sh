6ee847217411190aead3fafcea8cfc28f7d0a745
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2017-11-10 22:30:36 -0600

    bump to lede 17.01.4
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- settings.sh
+++ settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export genesisBuilder=lede-imagebuilder-17.01.2-ar71xx-generic.Linux-x86_64
-export genesisDownload=https://downloads.lede-project.org/releases/17.01.2/targets/ar71xx/generic/lede-imagebuilder-17.01.2-ar71xx-generic.Linux-x86_64.tar.xz
+export genesisBuilder=lede-imagebuilder-17.01.4-ar71xx-generic.Linux-x86_64
+export genesisDownload=https://downloads.lede-project.org/releases/17.01.4/targets/ar71xx/generic/lede-imagebuilder-17.01.4-ar71xx-generic.Linux-x86_64.tar.xz
 export buildProfile=archer-c7-v2
