4942d9c1c58a8bd390d9379e3a16fd9e02ec46ae
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2017-12-07 18:16:30 -0600

    Add git hash to image during build process
    This will help track down the exact code that was in a build.
    
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- build.sh
+++ build.sh
@@ -13,6 +13,9 @@
 #The LEDE image builder wants this umask set
 umask 022
 
+#this will put a marker in the image that will tell us what git commit was used to build the firmware.
+git log -1 --format="%H"> filesystem/etc/genesis39_git_hash
+
 #git doesn't save permissions, so explicitly set what we need here
 #this is specifically needed so that dnsmasq can read the hosts from /genesis39/hosts
 chmod -R ugo+rX filesystem/genesis39
