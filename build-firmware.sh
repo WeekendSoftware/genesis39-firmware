67787388c1504c04705df989095a2d002ea2bb12
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2018-12-26 22:54:23 -0600

    make debug opkg settings
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- build-firmware.sh
+++ build-firmware.sh
@@ -45,7 +45,7 @@ binfolder=`echo $(pwd)/dynamic-files/bin/$upstreamBuilder`
 
 pushd dynamic-files/$upstreamBuilder>/dev/null
 
-make image PROFILE="$upstreamProfile" PACKAGES="luci luci-app-sqm luci-app-ddns genesis39 genesis39-debug" FILES="../../filesystem/" BIN_DIR="$binfolder"
+make image PROFILE="$upstreamProfile" PACKAGES="luci luci-app-sqm luci-app-ddns genesis39 genesis39-opkg-dev genesis39-debug" FILES="../../filesystem/" BIN_DIR="$binfolder"
 
 popd>/dev/null
 echo Genesis 39: Build Complete
