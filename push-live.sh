0a5feea99cc31c425e92ac1cd0138b128b34a41f
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2020-02-09 22:27:15 -0600

    fix
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- push-live.sh
+++ push-live.sh
@@ -6,7 +6,7 @@ echo pushing latest firmware - `date`
 scp -o "StrictHostKeyChecking no" dynamic-files/bin/openwrt-imagebuilder-19.07.1-ath79-generic.Linux-x86_64/openwrt-19.07.1-ath79-generic-tplink_archer-a7-v5-squashfs-sysupgrade.bin $IP:/tmp/
 
 echo starting upgrade - `date`
-ssh -o "StrictHostKeyChecking no" $IP sysupgrade -v -n /tmp/openwrt-19.07.1-ath79-generic-tplink_archer-a7-v5-squashfs-sysupgrade.bin
+ssh -o "StrictHostKeyChecking no" $IP sysupgrade -n /tmp/openwrt-19.07.1-ath79-generic-tplink_archer-a7-v5-squashfs-sysupgrade.bin
 
 echo waiting for router to go down - `date`
 while ping -c1 $IP &>/dev/null
@@ -15,6 +15,7 @@ done
 
 echo waiting for router to come back up... - `date`
 
+sleep 20
 while ! ping -c1 $IP &>/dev/null
   do sleep 1
 done
