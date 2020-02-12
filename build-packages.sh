eee338fe3cd8f9a2d2d84fe84bc5a82d22d722ca
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2020-02-11 21:30:59 -0600

    create safe-dns package
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- build-packages.sh
+++ build-packages.sh
@@ -38,6 +38,7 @@ pushd dynamic-files/$upstreamSdk>/dev/null
 make package/genesis39/compile
 make package/genesis39-opkg/compile
 make package/genesis39-debug/compile
+make package/safe-dns/compile
 make package/index
 #make
 cd ..
---
+++ packages/net/safe-dns/Readme.md
@@ -0,0 +1,5 @@
+# Package: net/safe-dns
+
+This package configures dnsmasq to block adult websites on your router.
+
+The default settings are designed to be appropriate for most businesses and families.
