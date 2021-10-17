eb497657badbc3c724ec77abf827df88869b49d5
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2021-10-17 18:45:26 -0500

    bump to 21.02.0
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- settings.sh
+++ settings.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export genesisBuilder=openwrt-imagebuilder-21.02.0-rc4-ath79-generic.Linux-x86_64
-export genesisDownload=https://downloads.openwrt.org/releases/21.02.0-rc4/targets/ath79/generic/openwrt-imagebuilder-21.02.0-rc4-ath79-generic.Linux-x86_64.tar.xz
+export genesisBuilder=openwrt-imagebuilder-21.02.0-ath79-generic.Linux-x86_64
+export genesisDownload=https://downloads.openwrt.org/releases/21.02.0/targets/ath79/generic/openwrt-imagebuilder-21.02.0-ath79-generic.Linux-x86_64.tar.xz
 export buildProfile=tplink_archer-c7-v5
