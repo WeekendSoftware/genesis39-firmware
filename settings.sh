f9610f5c285f02d606a7b9ada2b8e8767389fc2e
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2022-03-18 18:56:40 -0500

    move version number to a variable
    Signed-off-by: Gregory L. Dietsche <gregory.dietsche@cuw.edu>

--- settings.sh
+++ settings.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-export genesisBuilder=openwrt-imagebuilder-21.02.2-ath79-generic.Linux-x86_64
-export genesisDownload=https://downloads.openwrt.org/releases/21.02.2/targets/ath79/generic/openwrt-imagebuilder-21.02.2-ath79-generic.Linux-x86_64.tar.xz
+export openwrtVersion=21.02.2
+export genesisBuilder=openwrt-imagebuilder-${openwrtVersion}-ath79-generic.Linux-x86_64
+export genesisDownload=https://downloads.openwrt.org/releases/${openwrtVersion}/targets/ath79/generic/openwrt-imagebuilder-${openwrtVersion}-ath79-generic.Linux-x86_64.tar.xz
 export buildProfile=tplink_archer-c7-v5
